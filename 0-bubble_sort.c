18fe75c753ad4bf054dd205ba38f98ea61996ce0
Goldeno10 <ibrahimmuhammad271@gmail.com>
2022-08-11 09:28:11 +0000

    Added bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,36 @@
+#include "sort.h"
+
+/**
+ *bubble_sort - Sorts an array of integers in ascending
+ *order using the Bubble sort algorithm
+ *@array: Array to be sorted
+ *@size: size of array
+ *Return: Nothing
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t n = size;
+	int flag;
+	size_t i, j;
+	int temp;
+
+	for (i = 0; i < n - 1; i++)
+	{
+		flag = 0;
+		for (j = 0; j < n - (i + 1); j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j + 1];
+				array[j + 1] = array[j];
+				array[j] = temp;
+				print_array(array, n);
+				flag = 1;
+			}
+		}
+		if (flag == 0)
+			break;
+
+	}
+}
--- sort.h
+++ sort.h
@@ -1,8 +1,7 @@
 #ifndef SORT_H
 #define SORT_H
 
-void print_array(const int *array, size_t size):
-void print_list(const listint_t *list):
+#include <stddef.h>
 
 /**
  * struct listint_s - Doubly linked list node
@@ -17,6 +16,9 @@ typedef struct listint_s
     struct listint_s *prev;
     struct listint_s *next;
 } listint_t;
+
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
 
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
